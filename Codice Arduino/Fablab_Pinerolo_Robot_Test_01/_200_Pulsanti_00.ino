98dcf7b94d67e5becea9f3f47cce939c3c3f740f
RoboticBoyer <giorgio.boiero@gmail.com>
2024-05-03 18:26:20 +0200

    aggiornamento sketch 01

---
+++ Codice Arduino/Fablab_Pinerolo_Robot_Test_01/_200_Pulsanti_00.ino
@@ -0,0 +1,60 @@
+// Función para manejar el botón
+void Button()
+{
+  //Serial.print("P1: ");  Serial.println(digitalRead(6)); //GB
+  
+  //if (digitalRead(6) == false) //originale
+    if (digitalRead(6) == true) //GB
+  {
+    delay(1);
+    //if (digitalRead(6) == true) //originale
+    if (digitalRead(6) == false) //GB
+    {
+      if (Taster == 0)
+      {
+        Taster = 1;
+        previousMillis3 = currentMillis;
+      }
+      else if ((Taster == 1) && (currentMillis - previousMillis3 < 250))
+      {
+        Taster = 2;
+      }
+    }
+  }
+
+
+
+if (Taster != old_Taster){ //GB
+   Serial.print("Taster: "); Serial.println(Taster);
+   old_Taster=Taster;
+}
+
+  if ((Taster == 1) && (currentMillis - previousMillis3 > 1000))
+  {
+    arrayStep += 1;
+    arrayMax = arrayStep;
+    record();
+    Taster = 0;
+    playmode = false;
+    Serial.print("Record Step: "); Serial.println(arrayStep);
+    digitalWrite(13, HIGH);
+    delay(100);
+    digitalWrite(13, LOW);
+  }
+  else if (Taster == 2)
+  {
+    arrayStep = 0;
+    playmode = true;
+    Taster = 0;
+    Step = 1;
+    Serial.println("playmode ");
+    data_out();
+    delay(250);
+    digitalWrite(13, LOW);
+  }
+
+  if (currentMillis - previousMillis3 > 2000)
+  {
+    Taster = 0;
+  }
+}
